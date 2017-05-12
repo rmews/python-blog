e690a11dd770b585fdbb6431ff8819ee8735666f
Robert Mews <robert.mews@gmail.com>
2017-05-11 23:35:53 -0500

    refactored models into own folder for modularity

---
+++ models/like.py
@@ -0,0 +1,8 @@
+from google.appengine.ext import db
+
+
+class Like(db.Model):
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_modified = db.DateTimeProperty(auto_now=True)
+    user_id = db.IntegerProperty(required=True)
+    post_id = db.IntegerProperty(required=True)
